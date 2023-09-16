d1ae467c043b3df005cb243ecd99ff457ea5c1bd
MrDiam0nd <vilmer.asker@gmail.com>
2023-09-16 13:50:11 +0200

    updated countdown
    nu räknar den ner tills det blir helg

--- countdown/script.js
+++ countdown/script.js
@@ -2,14 +2,31 @@ let days = document.getElementById("days");
 let hours = document.getElementById("hours");
 let minutes = document.getElementById("minutes");
 let seconds = document.getElementById("seconds");
-
-const targetDate = "16 Jun 2023";
-const targetTime = new Date(targetDate).getTime();
+let countdowndiv = document.getElementById("countdownDiv");
+let text = document.getElementById("text");
 
 function update(){
     let curentTime = new Date().getTime();
+    
+    let day = new Date().getDate();
+    let dayOfTheWeek = new Date().getDay();
+    
+    if(dayOfTheWeek == 0){
+        dayOfTheWeek = 7;
+    }
+    day += 5-dayOfTheWeek;
+    
+    let targetTime = new Date(new Date().getMonth()+1+" "+day+" "+(new Date().getYear()+1900)+" 15:10:00").getTime();
 
     milisecLeft = targetTime - curentTime;
+    
+    if(milisecLeft < 0){
+        countdowndiv.style.display = "none";
+        text.innerHTML = "It is the weekend right now!";
+    }else{
+        countdowndiv.style.display = "flex";
+        text.innerHTML = "Weekend";
+    }
 
     secondsLeft = Math.floor(milisecLeft / 1000);
     minutesLeft = Math.floor(secondsLeft/60);
